b44e93b147d49b5c0acb728015232e0755aee9ba
andreasa <andreas.altanis@gmail.com>
2022-03-30 13:43:46 +0200

    zad29

---
+++ suma-zmiennych.js
@@ -0,0 +1,8 @@
+let firstNumber = 15;
+let secondNumber = 222;
+function suma(num1,num2) {
+    let result = num1 + num2;
+    return result;
+}
+let wynik = suma(firstNumber,secondNumber)
+console.log(wynik)
